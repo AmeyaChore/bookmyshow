8953242047f30a7aa2e8cbe4493dafda4189c3b3
Chore <ameyachore475@gmail.com>
2024-02-06 15:50:09 +0530

    'ProxyAPIProject'

---
+++ FakeStoreProxyApi/FakeStoreProxyApi/src/main/java/com/proxyapi/FakeStoreProxyApi/FakeStoreProxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.proxyapi.FakeStoreProxyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreProxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeStoreProxyApiApplication.class, args);
+	}
+
+}
---
+++ FakeStoreProxyApi/FakeStoreProxyApi/src/test/java/com/proxyapi/FakeStoreProxyApi/FakeStoreProxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxyapi.FakeStoreProxyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreProxyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
